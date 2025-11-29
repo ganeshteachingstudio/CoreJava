2096c73f27daf9e150e233325b01c872ba0e60d9
ganeshteachingstudio <ganeshteachingstudio@gmail.com>
2025-11-29 13:39:17 +0530

    first code

---
+++ CoreJavaCourse/src/core/basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package core.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hellow World");
+	}
+
+}
---
+++ CoreJavaCourse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sadag
+ *
+ */
+module CoreJavaCourse {
+}
