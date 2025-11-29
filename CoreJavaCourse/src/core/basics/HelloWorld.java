f3173ebeab4e51584fedd459c89aa51756e849fd
ganeshteachingstudio <ganeshteachingstudio@gmail.com>
2025-11-29 14:00:46 +0530

    added comment line for Hellword.java

--- CoreJavaCourse/src/core/basics/HelloWorld.java
+++ CoreJavaCourse/src/core/basics/HelloWorld.java
@@ -1,10 +1,13 @@
-package core.basics;
+package core.basics;   // This defines the package name where this file belongs
 
+// This is a simple Java class named HelloWorld
 public class HelloWorld {
 
-	public static void main(String[] args) {
+    // main() method is the entry point of any Java program
+    public static void main(String[] args) {
 
-		System.out.println("Hellow World");
-	}
+        // This line prints the text "Hello World" to the console
+        System.out.println("Hello World");
+    }
 
 }
